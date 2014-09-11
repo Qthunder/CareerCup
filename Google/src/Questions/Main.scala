f1346ec70b3c77f12b14ec8dbb35be350f92d702
Gabriel Asman <Gabriel Asman>
2014-09-12 00:17:50 +0300

    Revert "Testing"
    This reverts commit 0e4f9a67ae71f638111a9f6b5dcd668d75af5507.

--- Google/src/Questions/Main.scala
+++ Google/src/Questions/Main.scala
@@ -3,7 +3,7 @@ package Questions
 object Main {
 
   def main(args: Array[String]): Unit = {
-     val smallestRange =( MinimumRange.getSmallestRange(List(List(4,10,15,24,26),List(0,9,12,20),List(5,18,20,30))));
+     val smallestRange =( MinimumRange.getSmallestRange(List(List(4,10,15,24,26),List(0,9,12,20),List(5,18,20,30))))
      println(smallestRange);
   }
 
